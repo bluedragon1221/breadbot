8903dc435e5cce9da0b5f2768cf2371ffe5b06dd
bluedragon1221 <96917990+bluedragon1221@users.noreply.github.com>
2022-06-21 15:35:35 -0500

    add select.py

---
+++ select.py
@@ -0,0 +1,12 @@
+import random as random
+
+people = [
+    'Collin Williams',
+    'Dylon Gentry',
+    'Sam Ricker',
+    'Luke Chase',
+    'Lukas Mefford',
+    'Cole Brekenridge'
+]
+
+print(random.choice(people))
